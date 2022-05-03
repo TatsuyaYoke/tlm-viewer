3392f7af32bcceea5fe8b740131a1b5cd5af7612
Yoke <tayok@synspective.com>
2022-05-03 15:19:27 +0900

    regexDateTime

--- types/index.ts
+++ types/index.ts
@@ -16,7 +16,8 @@ export const objectTypeSchema = z.record(dataTypeSchema)
 export const arrayObjectSchema = z.array(objectTypeSchema)
 export const objectArrayTypeSchema = z.record(z.array(dataTypeSchema))
 export const objectArrayTypeIncludeDateSchema = z.object({ DATE: z.array(z.string()) }).and(objectArrayTypeSchema)
-export const dateArraySchema = z.array(z.string())
+const regexDateTime = /^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01]) ([01][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]/
+export const dateArraySchema = z.array(z.string().regex(regexDateTime))
 
 export type ArrayObjectType = z.infer<typeof arrayObjectSchema>
 export type ObjectType = z.infer<typeof objectTypeSchema>
