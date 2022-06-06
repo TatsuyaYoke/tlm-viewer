abcd4bc5ea52c0981c916346e6826a51608d1a0d
Yoke <tayok@synspective.com>
2022-06-06 15:24:10 +0900

    useSize to handle maximize

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
-import { useRef } from 'react'
+import { useRef, useEffect } from 'react'
 
 import { Box, Container } from '@chakra-ui/react'
+import useSize from '@react-hook/size'
 import { useSetRecoilState } from 'recoil'
 
 import { isMaximizeState } from '@atoms/PlotSettingAtom'
 import { AppBar, GraphPlot, PlotSetting } from '@components'
-import { useResizeObserver } from '@hooks'
 
 export const App = () => {
   const headerHeight = '30px'
-  const appTopElement = useRef(null)
+  const target = useRef(null)
   const setIsMaximize = useSetRecoilState(isMaximizeState)
 
   const handleResize = async () => {
     setIsMaximize(await window.Main.isMaximize())
   }
 
-  useResizeObserver(appTopElement, handleResize)
+  const [width, height] = useSize(target)
+  useEffect(() => {
+    handleResize()
+  }, [width, height])
 
   return (
-    <Box ref={appTopElement} h="100vh">
+    <Box ref={target} h="100vh">
       {window.Main && <AppBar height={headerHeight} />}
       <Container maxW="container.2xl" p={0} pt={headerHeight} display="flex" minH="100vh">
         <PlotSetting width="450px" flexShrink={0} />
