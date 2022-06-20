aab9ea8c9fb5e80b00a76891785dd76486dc7e2c
Yoke <tayok@synspective.com>
2022-06-20 11:48:32 +0900

    delete @react-hook/size and use useWindowSize of usehooks-ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,27 @@
-import { useRef, useEffect } from 'react'
+import { useEffect } from 'react'
 
 import { Box, Container } from '@chakra-ui/react'
-import useSize from '@react-hook/size'
 import { useSetRecoilState } from 'recoil'
+import { useWindowSize } from 'usehooks-ts'
 
 import { isMaximizeState } from '@atoms/PlotSettingAtom'
 import { AppBar, GraphPlot, PlotSetting } from '@components'
 
 export const App = () => {
   const headerHeight = '30px'
-  const target = useRef(null)
   const setIsMaximize = useSetRecoilState(isMaximizeState)
 
   const handleResize = async () => {
     setIsMaximize(await window.Main.isMaximize())
   }
 
-  const [width, height] = useSize(target)
+  const { width, height } = useWindowSize()
   useEffect(() => {
     handleResize()
   }, [width, height])
 
   return (
-    <Box ref={target} h="100vh">
+    <Box>
       {window.Main && <AppBar height={headerHeight} />}
       <Container maxW="container.2xl" p={0} pt={headerHeight} display="flex" minH="100vh">
         <PlotSetting width="450px" flexShrink={0} />
